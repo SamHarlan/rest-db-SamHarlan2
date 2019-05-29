a3b44f25ba17258e3f63e15e387dd56ab72a3745
Sam Harlan <sams.email24@gmail.com>
2019-05-29 14:56:17 -0700

    edit to delete/update

--- models/friends-01.js
+++ models/friends-01.js
@@ -42,23 +42,26 @@ exports.findByName = function FindByNameHandler(name, done){
         });
 }
 
- exports.update = function FriendsUpdateHandler(id, birthDate, firstName, lastName, gender, phone, done) {
-    db.get().query(
-        'UPDATE friends SET birth_date = birthDate, first_name = firstName, last_name = lastName, gender = gender, phone = phone WHERE friend_id = ?', id,
-        function UpdateQueryHandler(err, result){
-            if (err)
-                return done(err);
-            done(null, result.insertId);
-        });
-}
+
 
 exports.delete = function FriendsDeleteHandler(id,done) {
 
     db.get().query(
-        'DELETE FROM friends WHERE friend_id = ?', id, 
+        'DELETE * FROM friends WHERE friend_id = ?', id, 
         function DeleteQueryHandler(err, result, fields){
             if (err)
                 return done(err);
             done(null, result, fields);
+        });
+}
+
+exports.update = function FriendsUpdateHandler(id, birthDate, firstName, lastName, gender, phone, done) {
+    var values = [new Date(birthDate).toISOString().slice(0, 19).replace('T', ' '), firstName, lastName, gender, phone];
+    db.get().query(
+        'UPDATE friends SET (birth_date, first_name, last_name, gender, phone) ' + 'VALUES (?,?,?,?,?)', values + 'WHERE friend_id = ?', id,
+        function UpdateQueryHandler(err, result){
+            if (err)
+                return done(err);
+            done(null, result.insertId);
         });
 }
