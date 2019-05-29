290c9822b9b7b1f85297f83256ed64a4c7d1dffa
Sam Harlan <sams.email24@gmail.com>
2019-05-29 16:59:43 -0700

    edit

--- friends-router-01.js
+++ friends-router-01.js
@@ -70,16 +70,27 @@ router.get('/friends', function FriendsGetHandler(request, response){
     });
 });
 
-router.get('/:id', function FriendsGetHandler(request, response){
-    friendModel.findById(function DoneGettingAll(err, result, fields){
-        if (err) {
-            console.log("Some error selecting all");
+router.get('/:id', function FriendsGetByIdHandler(request, response){
+    friendModel.findById(request.params.id, function DoneGettingById(err, result, fields){
+        if (err){
+            console.log("Some error finding by id");
             console.log(err);
-            response.write("Error Getting All");
-        } else {
-            console.log("Successfully retrieve all records (100)");
+            response.write("Error finding by id");
+        }else {
             response.json(result);
         }
+    });
+});
+
+router.get('/friends/:name', function FriendsGetByNameHandler(request, response){
+    friendModel.findByName(request.params.name, function DoneGettingByName(err, result, fields){
+        if (err){
+            console.log("Some error finding by name");
+            console.log(err);
+            response.write("Error finding by name");
+		} else {
+			response.json(result);
+		}
     });
 });
 
